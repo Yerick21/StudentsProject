45f927f1ccc8145701b1d0bdcd0143a578578e47
Yerick Gonzalez <yerickliou@gmail.com>
2024-11-21 17:10:10 -0600

    Updated students info coding

--- Students/mainProgram.cs
+++ Students/mainProgram.cs
@@ -1,4 +1,5 @@
-using System.Runtime.InteropServices;
+using System;
+using System.Collections.Generic;
 
 public class Program
 {
@@ -7,9 +8,9 @@ public class Program
         // Create Student objects
         var student1 = new Student(1, "John", "Doe", new DateTime(2000, 1, 6), "Computer Science", true);
         var student2 = new Student(2, "Jane", "Smith", new DateTime(2001, 2, 7), "Engineering", true);
-        var student3 = new Student(3, "Jane", "Smith", new DateTime(2002, 3, 8), "Engineering", true);
-        var student4 = new Student(3, "Jane", "Smith", new DateTime(2003, 4, 9), "Engineering", true);
-        var student5 = new Student(3, "Jane", "Smith", new DateTime(2004, 5, 10), "Engineering", true);
+        var student3 = new Student(3, "Jake", "Johnson", new DateTime(2002, 3, 8), "Engineering", false);
+        var student4 = new Student(4, "Sara", "Lee", new DateTime(2003, 4, 9), "Engineering", true);
+        var student5 = new Student(5, "Mike", "Davis", new DateTime(2004, 5, 10), "Engineering", false);
 
         // Create Course objects
         var class1 = new Course("Mathematics", "MATH101", "Professor James");
@@ -17,17 +18,40 @@ public class Program
 
         // Add courses to students
         student1.AddCourse(class1);  // student1 is enrolled in Mathematics
-        student2.AddCourse(class2);  // student2 is also enrolled in Computer Science
+        student2.AddCourse(class2);  // student2 is enrolled in Computer Science
+        student3.AddCourse(class2);  // student3 is also enrolled in Computer Science
+        student4.AddCourse(class1);  // student4 is enrolled in Mathematics
+        student5.AddCourse(class1);  // student5 is enrolled in Mathematics
 
-        // Example of displaying student info and their courses
-        if (student1.isEnrolled)
+        // Create a list of all students
+        List<Student> students = new List<Student> { student1, student2, student3, student4, student5 };
+
+        // Display all student information
+        foreach (var student in students)
         {
-            Console.WriteLine($"Name: {student1.fName} {student1.lName}, Major: {student1.Major}");
-            Console.WriteLine("Enrolled in the following courses:");
-
-            foreach (var course in student1.Courses)
+            if (student.isEnrolled)
             {
-                Console.WriteLine($"{course.ClassName} ({course.ClassCode}) - Instructor: {course.Instructor}");
+                Console.WriteLine($"Name: {student.fName} {student.lName}");
+                Console.WriteLine($"Student ID: {student.ID}");
+                Console.WriteLine($"Date of Birth: {student.DOB.ToShortDateString()}");
+                Console.WriteLine($"Major: {student.Major}");
+                Console.WriteLine($"Enrolled: {student.isEnrolled}");
+                Console.WriteLine("Enrolled in the following courses:");
+                
+
+                foreach (var course in student.Courses)
+                {
+                    Console.WriteLine($"- {course.ClassName} ({course.ClassCode}) - Instructor: {course.Instructor}");
+                }
+
+                Console.WriteLine(); // Add a blank line for better readability
+            }
+
+            else
+            {
+                Console.WriteLine($"Name: {student.fName} {student.lName} {student.ID} {student.DOB.ToShortDateString()} {student.Major},");
+                Console.WriteLine("Student has not been enrolled in any course");
+                Console.WriteLine();
             }
         }
     }
--- Students/obj/Debug/net8.0/Students.AssemblyInfo.cs
+++ Students/obj/Debug/net8.0/Students.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Students")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5e230954e943e0f4ac71a85b9075b8e15c344f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Students")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Students")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
